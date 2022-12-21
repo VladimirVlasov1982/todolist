395a0d52d86c285016cca1b9712f9cf548daf3ac
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2022-12-21 15:07:02 +0300

    added core application and user model

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Поможет создавать задачи и контролировать сроки их выполнения
 
 * стек (python3.10.4, Django, Postgres)
+***
+### Подготовка проекта
+Установите зависимости проекта
+```
+pip install -r requirements.txt
+```
+Установите docker-контейнер с уже готовой и настроенной СУБД:
+```
+docker run --name todolist_db -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+```
--- todolist/settings.py
+++ todolist/settings.py
@@ -1,3 +1,5 @@
+import environ
+import os
 """
 Django settings for todolist project.
 
@@ -12,20 +14,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z@=c(q5y0@qznfv+b)+9f4s(4j_4_@s2kxd4)6+s5$_nl%khup'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +86,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -116,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "core.User"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
