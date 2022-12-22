667afcde8f57082f660b78080166407fdb72bf7f
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2022-12-22 11:01:53 +0300

    added a user model to django admin

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
+    """
+    Модель пользователя
+    """
+
     def __str__(self):
         return self.username
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
