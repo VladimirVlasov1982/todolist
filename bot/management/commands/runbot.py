a794218da4408733f3ce3bfe0ca627bfff377249
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2023-02-06 16:55:06 +0300

    implemented a command to create a goal

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
 from bot.tg.dc import Message
-from goals.models import Goal
+from goals.models import Goal, GoalCategory
 from todolist import settings
 
 
@@ -16,6 +16,8 @@ class Command(BaseCommand):
         self.__tg_user: TgUser | None = None
         self.logger = logging.getLogger(__name__)
         self.logger.info('Bot start pooling')
+        self._state = None
+        self._category = None
 
     @property
     def tg_user(self) -> TgUser:
@@ -33,7 +35,7 @@ class Command(BaseCommand):
                     chat_id=item.message.chat.id,
                     defaults={'username': item.message.from_.username}
                 )
-                if self.tg_user.user_id:
+                if self.tg_user.user:
                     self._handle_verified_user(item.message)
                 else:
                     self._handle_unverified_user(item.message)
@@ -57,14 +59,36 @@ class Command(BaseCommand):
         match message.text:
             case '/goals':
                 self._handle_goals_command(message)
+            case '/create':
+                self._handle_get_category_list(message)
+                self._state = 'get_category'
+            case '/cancel':
+                self._state = None
             case _:
                 self._handle_message(message)
 
     def _handle_message(self, message: Message):
-        if message.text != '/goals':
-            self.tg_client.send_message(message.chat.id, 'Неизвестная команда')
 
-        self.logger.info(f'Пользователь отправил: {message.text}')
+        if self._state == 'get_category':
+            categories = list(
+                GoalCategory.objects.filter(user_id=self.tg_user.user).exclude(is_deleted=True).values_list('title'))
+            self.logger.info(f'{categories}')
+            cat = list(map(lambda x: x[0], categories))
+            self.logger.info(f'{cat}')
+            if message.text in cat:
+                self._category = GoalCategory.objects.get(title=message.text)
+                self.tg_client.send_message(message.chat.id, 'Введите название цели')
+                self._state = 'create_goal'
+                self.logger.info(f'{self._state}')
+            else:
+                self.tg_client.send_message(message.chat.id, 'Не верно выбрана категория')
+                self._state = None
+                self._handle_incorrect_category()
+
+        elif self._state == 'create_goal':
+            self._handle_create_goal(message, category=self._category)
+        else:
+            self.tg_client.send_message(message.chat.id, f'{message.text} не является командой')
 
     def _handle_goals_command(self, message: Message):
         goals: list[str] = list(
@@ -77,3 +101,35 @@ class Command(BaseCommand):
             chat_id=message.chat.id,
             text='\n'.join(goals) if goals else 'Целей не найдено'
         )
+
+    def _handle_get_category_list(self, message: Message):
+        category: list[str] = list(
+            GoalCategory.objects.filter(user_id=self.tg_user.user).exclude(is_deleted=True).values_list(
+                'title',
+                flat=True
+            )
+        )
+        if category:
+            self.tg_client.send_message(
+                chat_id=message.chat.id,
+                text='\nВыберите категорию:\n' + '\n'.join(category)
+            )
+            self._state = 'create_goal'
+        else:
+            self.tg_client.send_message(
+                chat_id=message.chat.id,
+                text='Категории не найдены'
+            )
+            self._state = None
+
+    def _handle_create_goal(self, message: Message, category: str):
+        goal = Goal.objects.create(
+            title=message.text,
+            category=category,
+            user_id=self.tg_user.user_id,
+        )
+        self.tg_client.send_message(message.chat.id, f'Цель {goal.title} успешно создана')
+        self._state = None
+
+    def _handle_incorrect_category(self):
+        self._state = 'get_category'
